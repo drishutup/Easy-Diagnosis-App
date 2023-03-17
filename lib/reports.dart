c8eac520f64704ca3453f8081381579429726a71
Anushkajain22 <102728955+Anushkajain22@users.noreply.github.com>
2023-03-17 18:41:43 +0530

    Add files via upload

---
+++ lib/reports.dart
@@ -0,0 +1,255 @@
+// vital sign page//
+import 'package:flutter/widgets.dart';
+// import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+      debugShowCheckedModeBanner: false, home: HealthMonitoringApp()),
+);
+
+class HealthMonitoringApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff7f9fb),
+      body: SafeArea(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.top,
+          children: <Widget>[
+            SizedBox(
+              height: 23.0,
+              width: 120.0,
+              child: Divider(
+                color: Color.fromARGB(255, 248, 247, 247),
+              ),
+            ),
+
+            Container(
+                child: Row(
+                  children: [
+                    Icon(Icons.computer, size: 40, color: Colors.black87),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Evaluation Report',
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 25.0,
+                        color: Color(0xff131010),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                )),
+            // Text(
+            //   'Evaluation Report',
+            //   style: TextStyle(
+            //     fontFamily: 'Pacifico',
+            //     fontSize: 30.0,
+            //     color: Color(0xff131010),
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            // Text(
+            //   'VITAL SIGNS MONITORING',
+            //   style: TextStyle(
+            //     fontFamily: 'Source Sans Pro',
+            //     color: Color(0xff45dccf),
+            //     fontSize: 20.0,
+            //     letterSpacing: 2.5,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            //
+
+            // SizedBox(
+            //   height: 0.2,
+            //   width: 20.0,
+            //   child: Divider(
+            //     color: Color.fromARGB(255, 4, 4, 4),
+            //     thickness: 2,
+            //   ),
+            // ),
+            SizedBox(
+              height: 20,
+              width: 50.0,
+              child: Divider(
+                color: Color(0xffffffff),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.line_weight,
+                  color: Color(0xff262727),
+                ),
+                title: Text(
+                  'weight: 80 Kg',
+                  style: TextStyle(
+                    color: Color(0xff262727),
+                    fontFamily: 'Pacifico',
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+            ),
+
+            // SizedBox(
+            //   height: 20.0,
+            //   width: 150.0,
+            //   child: Divider(
+            //     color: Colors.teal.shade100,
+            //   ),
+            // ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.directions_walk,
+                  color: Color(0xff262727),
+                ),
+                title: Text(
+                  'Steps: 395',
+                  style: TextStyle(
+                    color: Color(0xff262727),
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+            // SizedBox(
+            //   height: 20.0,
+            //   width: 150.0,
+            //   child: Divider(
+            //     color: Colors.teal.shade100,
+            //   ),
+            // ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.favorite,
+                  color: Color(0xff262727),
+                ),
+                title: Text(
+                  'Heart Rate: 80 BPM',
+                  style: TextStyle(
+                    color: Color(0xff262727),
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+            ),
+
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.ac_unit,
+                  color: Color(0xff262727),
+                ),
+                title: Text(
+                  'Body Temperature: 98.6°F',
+                  style: TextStyle(
+                    color: Color(0xff262727),
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.insert_chart,
+                  color: Color(0xff262727),
+                ),
+                title: Text(
+                  'Blood Pressure: 120/80 mmHg',
+                  style: TextStyle(
+                    color: Color(0xff262727),
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    // home:
+    // MyNavigationBar();
+    // );
+  }
+}
+
+// class MyNavigationBar extends StatefulWidget {
+//   MyNavigationBar({Key? key}) : super(key: key);
+
+//   @override
+//   _MyNavigationBarState createState() => _MyNavigationBarState();
+// }
+
+// class _MyNavigationBarState extends State<MyNavigationBar> {
+//   int _selectedIndex = 0;
+//   static const List<Widget> _widgetOptions = <Widget>[
+//     Text('Home Page',
+//         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+//     Text('Search Page',
+//         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+//     Text('Profile Page',
+//         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+//   ];
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//           title: const Text('Flutter BottomNavigationBar Example'),
+//           backgroundColor: Colors.green),
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//           items: const <BottomNavigationBarItem>[
+//             BottomNavigationBarItem(
+//                 icon: Icon(Icons.home),
+//                 // title: Text('Home'),
+//                 backgroundColor: Colors.green),
+//             BottomNavigationBarItem(
+//                 icon: Icon(Icons.search),
+//                 // title: Text('Search'),
+//                 backgroundColor: Colors.yellow),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.person),
+//               // title: Text('Profile'),
+//               backgroundColor: Colors.blue,
+//             ),
+//           ],
+//           type: BottomNavigationBarType.shifting,
+//           currentIndex: _selectedIndex,
+//           selectedItemColor: Colors.black,
+//           iconSize: 40,
+//           onTap: _onItemTapped,
+//           elevation: 5),
+//     );
+//   }
+// }
