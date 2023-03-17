6e00c49c3483d189fe8c794a7fc79bd9725c1d13
Anushkajain22 <102728955+Anushkajain22@users.noreply.github.com>
2023-03-17 18:44:00 +0530

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -120,31 +120,3 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.pushNamed(context, '/home_page');
   }
 }
-
-// class pathPainter extends CustomPainter {
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     var paint = Paint();
-//     paint.color = path1Color;
-//     paint.style = PaintingStyle.fill;
-//     var path = Path();
-//     path.moveTo(0, size.height * 0.4);
-//     path.quadraticBezierTo(size.width * 0.35, size.height * 0.40,
-//         size.width * 0.58, size.height * 0.6);
-//     path.quadraticBezierTo(size.width * 0.72, size.height * 0.8,
-//         size.width * 0.92, size.height * 0.8);
-//     path.quadraticBezierTo(
-//         size.width * 0.98, size.height * 0.8, size.width, size.height * 0.82);
-//     path.lineTo(size.width, size.height);
-//     path.lineTo(0, size.height);
-//     path.close();
-//     canvas.drawPath(path, paint);
-//   }
-
-//   @override
-//   bool shouldRepaint(CustomPainter oldDelegate) {
-//     // TODO: implement shouldRepaint
-//     return true;
-//   }
-// }
-
